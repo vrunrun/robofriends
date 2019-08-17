bf90799afbc0b1c11b41fbf062143c2be9bfa5f4
kl <kl@debian>
2019-08-17 12:30:20 +0800

    Use api to get robot info

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, {Component} from 'react';
 import CardList from './CardList';
-import {robots} from './robots';	// non-default export, destructuring required
 import SearchBox from './SearchBox';
 import './App.css';
 
@@ -10,9 +9,18 @@ class App extends Component {
 		super();
 
 		this.state = {
-			robots: robots,
+			robots: [],
 			searchField: ''
 		}
+	}
+
+	// Life cycle hook
+	// Part of React, no arrow function required
+	componentDidMount() {
+		// Dynamically set robots array via fetch api
+		fetch('https://jsonplaceholder.typicode.com/users')
+		.then(response => response.json())
+		.then(robots => this.setState({robots: robots}));
 	}
 
 	// Event handler
@@ -29,15 +37,20 @@ class App extends Component {
 			return robot.name.toLowerCase().includes(this.state.searchField.toLowerCase());
 		});
 
-		return (
-			<div className="tc">
-				<h1 className="f1">RoboFriends</h1>
-				{/* Pass event handler and to SearchBox*/}
-				<SearchBox searchChange={this.onSearchChange}/>
-				{/* Pass filtered list of robots to CardList*/}
-				<CardList robots={filteredRobots}/>			
-			</div>
-		);
+		// Display loading message
+		if (this.state.robots.length === 0) {
+			return <div><h1>Loading</h1></div>
+		} else { 
+			return (
+				<div className="tc">
+					<h1 className="f1">RoboFriends</h1>
+					{/* Pass event handler and to SearchBox*/}
+					<SearchBox searchChange={this.onSearchChange}/>
+					{/* Pass filtered list of robots to CardList*/}
+					<CardList robots={filteredRobots}/>			
+				</div>
+			);
+		}
 	}
 }
 
