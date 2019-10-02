dde0e581baa950bab6566d3ce8bab4d2dd8fd3af
kl <kl@debian>
2019-10-02 15:31:46 +0800

    minor change

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import * as serviceWorker from './serviceWorker';
-
 import 'tachyons';	// CSS file. OK to import once
 import App from './containers/App';
 
